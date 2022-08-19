5dc042ce56de2aecbd6212517272745a8b8214e3
raflemin <raflemin@gmail.com>
2022-08-19 11:24:50 -0700

    add api key, url, payload

--- fin_classes.py
+++ fin_classes.py
@@ -3,3 +3,7 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+URL = 'https://financialmodelingprep.com/api/v3/'
+payload = {"limit": 120, "apikey": API_KEY}
