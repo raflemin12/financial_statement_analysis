ba31a9b10ba12716384b98dc7e23aa5fcc385faa
raflemin <raflemin@gmail.com>
2022-08-19 11:52:54 -0700

    working get and response

--- fin_classes.py
+++ fin_classes.py
@@ -5,5 +5,8 @@ import requests
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-URL = 'https://financialmodelingprep.com/api/v3/'
+URL = 'https://financialmodelingprep.com/api/v3/income-statement/NKE'
 payload = {"limit": 120, "apikey": API_KEY}
+
+r = requests.get(URL, payload)
+print(r.json())
