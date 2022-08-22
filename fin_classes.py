0f5c97bb95b6e2e6125097c1e8a04b96fec1e9ed
raflemin <raflemin@gmail.com>
2022-08-22 16:30:33 -0700

    explore FMP JSON structure

--- fin_classes.py
+++ fin_classes.py
@@ -9,4 +9,10 @@ URL = 'https://financialmodelingprep.com/api/v3/income-statement/NKE'
 payload = {"limit": 120, "apikey": API_KEY}
 
 r = requests.get(URL, payload)
-print(r.json())
+json = r.json()
+ebitda_dict = {}
+for year in json:
+    ebitda_dict[year['calendarYear']] = year['ebitda']
+print(ebitda_dict)
+
+# ebitda (income statement), roic(financial metric), roe (financial metric), earningsyield (financial metric), freecashflow (cashflow statement)
