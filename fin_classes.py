a87fcd3c8996359bbfdc54105bbe25d967007330
Ricky Fleming <raflemin@gmail.com>
2022-08-30 14:23:27 -0700

    create json_to_dict function

--- fin_classes.py
+++ fin_classes.py
@@ -11,15 +11,15 @@ class DataJson:
 
     def __init__(self, stock_symbol: str):
         # Validations of received arguments
-        assert len(stock_symbol) <= 5, 'Your stock symbol is too long'
+        assert len(stock_symbol) > 5, 'Your stock symbol is too long'
         
         # Assign to self object
         self.stock_symbol = stock_symbol
         self.financial_statement = ['income-statement', 'cash-flow-statement', 'key-metrics', 'balance-sheet-statement']
-        self.income_request_dict = self.get_json_data(self.set_url(self.financial_statement[0]))
-        self.key_metric_request_dict = self.get_json_data(self.set_url(self.financial_statement[1]))
-        self.cashflow_request_dict = self.get_json_data(self.set_url(self.financial_statement[2]))
-        self.balance_request_dict = self.get_json_data(self.set_url(self.financial_statement[3]))
+        self.income_request_json = self.get_json_data(self.set_url(self.financial_statement[0]))
+        self.key_metric_request_json = self.get_json_data(self.set_url(self.financial_statement[1]))
+        self.cashflow_request_json = self.get_json_data(self.set_url(self.financial_statement[2]))
+        self.balance_request_json = self.get_json_data(self.set_url(self.financial_statement[3]))
 
     def set_url(self, financial_statement: str):
         return f'{self.BASE_URL}/{financial_statement}/{self.stock_symbol}'
@@ -32,5 +32,15 @@ class DataJson:
             return 'Something went wrong with the API request'
 
 def json_to_dict(json_data: list):
-    statement_dict = {year['calendarYear']: year for year in json_data}
+    # statement_dict = {year['calendarYear']: year for year in json_data}
+    statement_dict = {}
+
+    for year in json_data:
+        statement_dict[year['calendarYear']] = {}
+        financials = year.items()[7:]
+        for point in financials:
+            statement_dict[year['calendarYear']][point[0]] = point[1]
     return statement_dict
+
+nke = DataJson('NKE')
+print(json_to_dict(nke.income_request_json))
