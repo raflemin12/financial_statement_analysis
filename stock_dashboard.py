2f3d2e1cc7fe381635388caa0e3409522f3a71e8
Ricky Fleming <raflemin@gmail.com>
2022-09-06 13:08:40 -0700

    load statement data into streamlist app

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from fin_classes import DataJson, json_to_dict, dict_to_dataframe
 
 st.title('Company Financials Dashboard')
+
+dis = DataJson('DIS')
+dis_income_statements_json = json_to_dict(dis.income_request_json)
+dis_income_statements_df = dict_to_dataframe(
+    dis_income_statements_json[1],
+    dis_income_statements_json[0])
+
+st.write(dis_income_statements_df)
