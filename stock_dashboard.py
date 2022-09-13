2f7896688ca1183ce99940c58cd71974f1c600d4
Ricky Fleming <raflemin@gmail.com>
2022-09-13 12:01:08 -0700

    allow user to enter stock symbol

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -4,7 +4,12 @@ from fin_classes import DataJson, json_to_dict, dict_to_dataframe, clean_rows
 st.title('Company Financials Dashboard')
 
 # cache JSON data
-dis = DataJson('DIS')
+stock_symbol = st.text_input(
+    "1) What stock would you like to analyze?", 
+    value= "Enter stock ticker"
+)
+
+stock= DataJson(stock_symbol)
 
 financial_statement_options = st.selectbox(
     "1) What financial statement would you like to analyze?",
@@ -12,38 +17,38 @@ financial_statement_options = st.selectbox(
 )
 # load appropriate data depending on selection
 if financial_statement_options == 'Income Statement':
-    dis_income_statements_json = json_to_dict(dis.income_request_json)
-    dis_income_statements_df = clean_rows(
+    stock_income_statements_json = json_to_dict(stock.income_request_json)
+    stock_income_statements_df = clean_rows(
         dict_to_dataframe(
-            dis_income_statements_json[1],
-            dis_income_statements_json[0]
+            stock_income_statements_json[1],
+            stock_income_statements_json[0]
             )
         )
-    st.write(dis_income_statements_df)
+    st.write(stock_income_statements_df)
 elif financial_statement_options == 'Balance Sheet':
-    dis_balance_json = json_to_dict(dis.balance_request_json)
-    dis_balance_df = clean_rows(
+    stock_balance_json = json_to_dict(stock.balance_request_json)
+    stock_balance_df = clean_rows(
         dict_to_dataframe(
-            dis_balance_json[1],
-            dis_balance_json[0]
+            stock_balance_json[1],
+            stock_balance_json[0]
             )
         )
-    st.write(dis_balance_df)
+    st.write(stock_balance_df)
 elif financial_statement_options == 'Cashflow Statement':
-    dis_cashflow_json = json_to_dict(dis.cashflow_request_json)
-    dis_cashflow_df = clean_rows(
+    stock_cashflow_json = json_to_dict(stock.cashflow_request_json)
+    stock_cashflow_df = clean_rows(
         dict_to_dataframe(
-            dis_cashflow_json[1],
-            dis_cashflow_json[0]
+            stock_cashflow_json[1],
+            stock_cashflow_json[0]
             )
         )
-    st.write(dis_cashflow_df)
+    st.write(stock_cashflow_df)
 else:
-    dis_key_metric_json = json_to_dict(dis.key_metric_request_json)
-    dis_key_metric_df = clean_rows(
+    stock_key_metric_json = json_to_dict(stock.key_metric_request_json)
+    stock_key_metric_df = clean_rows(
         dict_to_dataframe(
-            dis_key_metric_json[1],
-            dis_key_metric_json[0]
+            stock_key_metric_json[1],
+            stock_key_metric_json[0]
             )
         )
-    st.write(dis_key_metric_df)
+    st.write(stock_key_metric_df)
